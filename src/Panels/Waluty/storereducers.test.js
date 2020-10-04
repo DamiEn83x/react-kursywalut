1a16c2de25bac41d43ecfd072450564c98b64de8
DamiEn83x <62798664+DamiEn83x@users.noreply.github.com>
2020-10-04 16:18:50 +0200

    another good test

--- src/Panels/Waluty/storereducers.test.js
+++ src/Panels/Waluty/storereducers.test.js
@@ -43,32 +43,42 @@ describe("Test Reducers,actions nad states in ReduxStore", () => {
     expect(SelectedDateTo(state)).toEqual("2020-06-01");
   });
 
-  it("test load proper  ref currencies", () => {
+  it("test load proper  ref currencies", (done) => {
     const MockedFetchFuncion = jest.fn().mockReturnValue(
       new Promise((resolve, reject) => {
         resolve(
-          JSON.stringify([
-            {
-              table: "A",
-              no: "193/A/NBP/2020",
-              effectiveDate: "2020-10-02",
-              rates: [
-                { currency: "bat (Tajlandia)", code: "THB", mid: 0.1213 },
-                { currency: "dolar amerykański", code: "USD", mid: 3.8366 },
-                { currency: "dolar australijski", code: "AUD", mid: 2.7425 }
-              ]
-            },
-            {
-              table: "B",
-              no: "039/B/NBP/2020",
-              effectiveDate: "2020-09-30",
-              rates: [
-                { currency: "afgani (Afganistan)", code: "AFN", mid: 0.050061 },
-                { currency: "ariary (Madagaskar)", code: "MGA", mid: 0.000991 },
-                { currency: "balboa (Panama)", code: "PAB", mid: 3.8658 }
-              ]
-            }
-          ])
+          new Response(
+            JSON.stringify([
+              {
+                table: "A",
+                no: "193/A/NBP/2020",
+                effectiveDate: "2020-10-02",
+                rates: [
+                  { currency: "bat (Tajlandia)", code: "THB", mid: 0.1213 },
+                  { currency: "dolar amerykański", code: "USD", mid: 3.8366 },
+                  { currency: "dolar australijski", code: "AUD", mid: 2.7425 }
+                ]
+              },
+              {
+                table: "B",
+                no: "039/B/NBP/2020",
+                effectiveDate: "2020-09-30",
+                rates: [
+                  {
+                    currency: "afgani (Afganistan)",
+                    code: "AFN",
+                    mid: 0.050061
+                  },
+                  {
+                    currency: "ariary (Madagaskar)",
+                    code: "MGA",
+                    mid: 0.000991
+                  },
+                  { currency: "balboa (Panama)", code: "PAB", mid: 3.8658 }
+                ]
+              }
+            ])
+          )
         );
       })
     );
@@ -77,9 +87,30 @@ describe("Test Reducers,actions nad states in ReduxStore", () => {
     DisableMockFetch();
 
     expect(MockedFetchFuncion.mock.calls.length).toBe(1);
-    const state = store.getState();
-    expect(CurrencyItemsAllChecks(state)).toEqual({ code: "PLN", table: "A" });
-    expect(CurrencyItemsAllLookup(state)).toEqual({ code: "PLN", table: "A" });
+
+    setTimeout(() => {
+      try {
+        const state = store.getState();
+        expect(CurrencyItemsAllChecks(state)).toEqual([
+          { code: "PLN", name: "Polski złoty", table: "A" },
+          { code: "THB", name: "bat (Tajlandia)", table: "A" },
+          { code: "USD", name: "dolar amerykański", table: "A" },
+          { code: "AUD", name: "dolar australijski", table: "A" }
+        ]);
+        expect(CurrencyItemsAllLookup(state)).toEqual([
+          { code: "PLN", name: "Polski złoty", table: "A" },
+          { code: "THB", name: "bat (Tajlandia)", table: "A" },
+          { code: "USD", name: "dolar amerykański", table: "A" },
+          { code: "AUD", name: "dolar australijski", table: "A" },
+          { code: "AFN", name: "afgani (Afganistan)", table: "B" },
+          { code: "MGA", name: "ariary (Madagaskar)", table: "B" },
+          { code: "PAB", name: "balboa (Panama)", table: "B" }
+        ]);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    }, 300);
   });
 
   it("test states to api parameters", () => {
